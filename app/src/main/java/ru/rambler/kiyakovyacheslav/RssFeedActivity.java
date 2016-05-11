d47aa03fa5625322e6e35a728bd47d7a7a6d34f0
vyacheslav.kiyako <vyacheslav.kiyako@dsr-company.com>
2016-05-11 23:08:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/rambler/kiyakovyacheslav/RssFeedActivity.java
@@ -0,0 +1,13 @@
+package ru.rambler.kiyakovyacheslav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RssFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rss_feed_layout);
+    }
+}
